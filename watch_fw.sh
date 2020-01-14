73e0a75c5b62f9fc2beb60e4a6f49073425fc378
unman <unman@thirdeyesecurity.org>
2020-01-14 12:10:58 +0000

    Use a single shell script to update nft tables Bump version

---
+++ watch_fw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /usr/lib/3isec-tor/nft
+nft list ruleset > ruleset
+nft list table nat > nat
+nft list table qubes-firewall > qubes
+/usr/lib/3isec-tor/nft/update_nat.awk nat qubes > newnat
+/usr/lib/3isec-tor/nft/update_ruleset.awk  ruleset > newruleset
+nft -f newruleset
+rm ruleset nat newnat newruleset
+sleep 30
+while true; 
+do
+nft list table qubes-firewall > newqubes
+cmp -s qubes newqubes  || ./update_nft.sh 
+rm newqubes
+sleep 30
+done
